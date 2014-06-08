007375d4cf0c492f2dddde5240200d650f238187
Justin Lewsader <jlewsader@gmail.com>
2014-06-08 15:29:41 -0400

    Change back to built in contact form. Using SimpleForm API to send POST to email.

--- js/form.js
+++ js/form.js
@@ -1,18 +1,18 @@
 $(function() {
-	$("form").submit(function() {  
-    // we want to store the values from the form input box, then send via ajax below  
-    var name     = $('#name').attr('value');  
-    var email     = $('#email').attr('value');  
-    var msg     = $('#message').attr('value');  
-        $.ajax({  
-            type: "POST",  
-            url: "/gdform.php",  
-            data: "name="+ name +"& email="+ email +"& message=" + msg,  
-            success: function(){  
-                $('form').hide(function(){$('div#thankYou').fadeIn();});  
+	$("form").submit(function() {
+    // we want to store the values from the form input box, then send via ajax below
+    var name     = $('#name').attr('value');
+    var email     = $('#email').attr('value');
+    var msg     = $('#message').attr('value');
+        $.ajax({
+            dataType: "jsonp",
+            url: "http://getsimpleform.com/messages/ajax?form_api_token=f95814c1ce0d9fef8217eff5bc10af14",
+            data: "name="+ name +"& email="+ email +"& message=" + msg,
+            success: function(){
+                $('form').hide(function(){$('div#thankYou').fadeIn();});
   
-            }  
-        });  
-    return false;  
-    });  
+            }
+        });
+    return false;
+    });
 });
